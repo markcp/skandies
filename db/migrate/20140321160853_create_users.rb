25c269ea2246b9a68e6315e8fd1a5c5468bb3de3
Mark Pittillo <mark-pittillo@uiowa.edu>
2014-05-04 14:09:51 -0500

    Add admin boolean to User

--- db/migrate/20140321160853_create_users.rb
+++ db/migrate/20140321160853_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :email
+      t.boolean :admin, default: false
 
       t.timestamps
     end
