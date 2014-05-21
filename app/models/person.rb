a8c73efeaab2013bb255a2e48aa4ae96e03eb85a
Mark Pittillo <mark-pittillo@uiowa.edu>
2014-05-21 10:56:33 -0500

    Validate last_name_first

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,6 @@ class Person < ActiveRecord::Base
   has_many :credits
 
   validates :last_name, presence: true
-  validates :gender, inclusion: [ 'M', 'F', 'O' ]
+  validates :gender, inclusion: [ 'M', 'F', 'O' ], allow_nil: true
   validates :last_name_first, inclusion: [ true, false ]
 end
