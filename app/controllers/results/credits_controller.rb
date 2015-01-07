4d37a229be22de925c9b1c739ae2d51e537c7398
Mark Pittillo <mark-pittillo@uiowa.edu>
2015-01-07 16:20:56 -0600

    Fix typos in scene and credit votes display

--- app/controllers/results/credits_controller.rb
+++ app/controllers/results/credits_controller.rb
@@ -1,6 +1,7 @@
 class Results::CreditsController < ApplicationController
 
   def show
+    @year = Year.get_display_year(params[:year])
     @credit = Credit.find(params[:id])
     @category = @credit.category
     @votes = @credit.votes.by_points_and_user_name
--- app/controllers/results/scenes_controller.rb
+++ app/controllers/results/scenes_controller.rb
@@ -1,8 +1,9 @@
 class Results::ScenesController < ApplicationController
 
   def show
+    @year = Year.get_display_year(params[:year])
     @scene = Scene.find(params[:id])
     category = Category.where(name: "scene").first
-    @votes = @scene.votes.by_points_and_user_name(category)
+    @votes = @scene.votes.by_category_and_points_and_user_name(category)
   end
 end
